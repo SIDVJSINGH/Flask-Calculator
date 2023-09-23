65a7f2fb48c7b7c94ce944edf08c78b17dc5adf3
SIDVJSINGH <sidvjsingh@gmail.com>
2023-09-24 00:16:52 +0530

    all files

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,12 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 # Database connection
-db = PyMongo(app, uri="mongodb://localhost:27017/<database>")
+# local server connection with flask app
+app.config["MONGO_URI"] = "mongodb+srv://sidvjsingh:qns6OsGz45N9rouO@flaskapp.0o6otdg.mongodb.net/calculator"
+
+# Mongodb Object declaration
+mongo = PyMongo(app)
+# local database collection 
+db = mongo.db.history
 
 from application import routes
